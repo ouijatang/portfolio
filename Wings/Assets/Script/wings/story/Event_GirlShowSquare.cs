ec932ccc767d7c5369bc25127082382ff4cef598
Aieylisyh <Aieylisyh@users.noreply.github.com>
2023-02-14 12:51:42 +0800

    wip

---
+++ Wings/Assets/Script/wings/story/Event_GirlShowSquare.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using com;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class Event_GirlShowSquare : EventTrigger
+{
+    public CanvasObject dragBg;
+    public CharacterActor walker;
+    public Image squareBackground;
+    public RectTransform wholeImgRect;
+    public TownSquare townSquare;
+
+    protected override void OnTrigger()
+    {
+        SoundSystem.instance.Play("createCanvas");
+        dragBg.enabled = false;
+        walker.StopWalk();
+        squareBackground.DOFade(1, 0.5f);
+        wholeImgRect.DOAnchorPos(new Vector2(-1005, -114), 3f);
+        wholeImgRect.DOScale(0.6f, 3f);
+
+        townSquare.ShowPeople();
+    }
+}
