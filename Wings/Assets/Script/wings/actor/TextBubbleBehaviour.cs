d9ae7d40288889042f7dea9ca9bf37cb149e7958
Aieylisyh <Aieylisyh@users.noreply.github.com>
2023-02-15 17:43:52 +0800

    wip

---
+++ Wings/Assets/Script/wings/actor/TextBubbleBehaviour.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using System.Collections;
+
+public class TextBubbleBehaviour : MonoBehaviour
+{
+    public enum MoveAnim
+    {
+        None,
+        Shake,
+        Float,
+    }
+
+    public enum FadeAnim
+    {
+        None,
+        Alpha,
+    }
+    public MoveAnim moveAnim;
+    public FadeAnim fadeAnim;
+
+    public float shakeStrength = 4;
+
+    public float stayTime;
+    public Image img;
+
+    // Use this for initialization
+    void Start()
+    {
+        switch (moveAnim)
+        {
+            case MoveAnim.None:
+                break;
+            case MoveAnim.Shake:
+                transform.DOShakePosition(stayTime, shakeStrength, 8).SetEase(Ease.InCubic);
+                break;
+            case MoveAnim.Float:
+                var y = transform.position.y;
+                transform.DOMoveY(y + 15, stayTime).SetEase(Ease.InOutCubic);
+                break;
+        }
+
+        StartCoroutine(EndAction());
+    }
+
+    IEnumerator EndAction()
+    {
+        yield return new WaitForSeconds(stayTime + 0.1f);
+
+        switch (fadeAnim)
+        {
+            case FadeAnim.None:
+                Destroy(gameObject);
+                break;
+            case FadeAnim.Alpha:
+                img.DOFade(0, 0.4f).OnComplete(() => { Destroy(gameObject); });
+                break;
+        }
+    }
+}
---
+++ Wings/Assets/Script/wings/story/GameStart_girlsRun.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStart_girlsRun : MonoBehaviour
+{
+    public GameObject bubble1;
+    public GameObject bubble2;
+    public CanvasObject dragBg;
+    public CharacterActor walker;
+
+    public float t1 = 0.8f;
+    public float t2 = 2.8f;
+    public float t3 = 3.5f;
+
+    void Start()
+    {
+        StartCoroutine(Bubble1(t1));
+        StartCoroutine(Bubble2(t2));
+        StartCoroutine(EnableWalk(t3));
+    }
+
+    IEnumerator Bubble1(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        bubble1.SetActive(true);
+    }
+
+    IEnumerator Bubble2(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        bubble2.SetActive(true);
+    }
+
+    IEnumerator EnableWalk(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        dragBg.enabled = true;
+        //walker.StopWalk();
+    }
+}
