ce718fcc731e00e43025ebbc0bebe01f384dffad
Aieylisyh <Aieylisyh@users.noreply.github.com>
2023-02-17 12:07:34 +0800

    wip

---
+++ Wings/Assets/Script/wings/story/MemoryBridge.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class MemoryBridge : MonoBehaviour
+{
+    public RectTransform rect;
+    public Vector2 startAnchoredPos;
+    public Vector2 endAnchoredPos;
+    public float startScale;
+    public float endScale;
+
+    public float t_bridge;
+    public float t_girls;
+    public float t_scissors;
+
+    public Image imgBridge;
+    public Image imgGirls;
+    public Image imgScissors;
+
+    public void StartShowMemory()
+    {
+        rect.anchoredPosition = startAnchoredPos;
+        rect.transform.localScale = startScale * Vector3.one;
+
+        rect.DOScale(endScale, 4f).SetEase(Ease.InOutCubic);
+        rect.DOAnchorPos(endAnchoredPos, 4f).SetEase(Ease.InOutCubic);
+
+        StartCoroutine(ShowBridge());
+        StartCoroutine(ShowGirls());
+        StartCoroutine(ShowScissors());
+    }
+
+    IEnumerator ShowBridge()
+    {
+        yield return new WaitForSeconds(t_bridge);
+        imgBridge.DOFade(1, 1);
+    }
+
+    IEnumerator ShowGirls()
+    {
+        yield return new WaitForSeconds(t_girls);
+        imgGirls.DOFade(1, 1);
+    }
+
+    IEnumerator ShowScissors()
+    {
+        yield return new WaitForSeconds(t_scissors);
+        imgScissors.DOFade(1, 1);
+    }
+}
